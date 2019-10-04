a29517995d8468ff7beca31f9383809171647e04
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-04 08:27:27 +0100

    scaffold app class component and get the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const playerApi = "http://localhost:5000/api/players";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    Axios.get(playerApi)
+      .then(response => {
+        this.setState({ players: response.data });
+        console.log(response.data);
+      })
+
+      .catch(error => console.log(error.message));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Players</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
